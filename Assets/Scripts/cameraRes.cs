0bc0000f4b91d177cd9b29a4a1a922d78ca5fb40
basse747 <basebulldozer@gmail.com>
2016-11-29 16:30:02 +0100

    Camera res
    Added a camera resolution script.

---
+++ Assets/Scripts/cameraRes.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraRes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float targetaspect = 16.0f / 9.0f;
+
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        float scaleheight = windowaspect / targetaspect;
+
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+	}
+}
