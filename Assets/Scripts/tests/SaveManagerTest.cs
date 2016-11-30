bb95954f84a8abd9bec3c7c11b4fdc74c074e253
Giolegrand <g.le.grand@hotmail.com>
2016-11-30 15:12:40 +0100

    changed logic
    Changed some logic in the SaveManagerTest still have to figure out how
    to create an dynamic thing.

--- Assets/Scripts/tests/SaveManagerTest.cs
+++ Assets/Scripts/tests/SaveManagerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using Core;
@@ -13,7 +14,10 @@ public class SaveManagerTest : CoreClass
 		// the required Update method
 	}
 
-	public void reset(){
+	/**
+	 * @Return dynamic stats
+	 * */
+	public dynamic reset(){
 		PlayerPrefs.SetFloat("gold", 0f);
 		PlayerPrefs.SetFloat("goldPerClick", 1f);
 		PlayerPrefs.SetFloat("goldPerSec", 0f);
@@ -28,9 +32,58 @@ public class SaveManagerTest : CoreClass
 		PlayerPrefs.SetInt("diamonds", 0);
 		PlayerPrefs.SetInt("purchasedAds", 0);
 		PlayerPrefs.Save();
-		StartCoroutine(ShowMessage("The game has just been reset.", 10));
-		load();
-		baseGame.Update();
-		Debug.Log("Reset game.");
+		return load("The game has just been reset.");
+	}
+
+	/**
+	 * @Parameter(name="stats", type="dynamic")
+	 * @Return dynamic stats
+	 * */
+	public dynamic save(dynamic stats)
+	{
+		PlayerPrefs.SetFloat("gold",(float)stats.gold);
+		PlayerPrefs.SetFloat("goldPerClick", stats.goldPerClick);
+		PlayerPrefs.SetFloat("goldPerSec", stats.goldPerSec);
+		PlayerPrefs.SetFloat("clicks", stats.clicks);
+		PlayerPrefs.SetFloat("goldMade", stats.goldMade);
+		PlayerPrefs.SetInt("diamonds", stats.diamonds);
+		PlayerPrefs.SetInt("purchasedAds", stats.boughtAds);
+		PlayerPrefs.SetInt("item_number1", stats.count);
+		PlayerPrefs.SetInt("item_number2", stats.count);
+		PlayerPrefs.SetInt("item_number3", stats.count);
+		PlayerPrefs.SetInt("upgrade_number1", stats.count);
+		PlayerPrefs.SetInt("upgrade_number2", stats.count);
+		PlayerPrefs.SetInt("upgrade_number3", stats.count);
+		PlayerPrefs.Save();
+		stats.Message = "The game has just been saved.";
+		return stats;
+	}
+
+	/**
+	 * @Parameter(name="message", type="string", default=null)
+	 * @Return dynamic stats
+	 * */
+	public dynamic load(string message = null)
+	{
+		/**
+		 *  PlayerPrefs.Get*(string value, string/int/float defaultValue);
+		 * */
+		dynamic stats;
+		stats.Gold = PlayerPrefs.GetFloat("gold");
+		stats.GoldPClick = PlayerPrefs.GetFloat ("goldPerClick", 0f); 
+		stats.GoldPSec = PlayerPrefs.GetFloat ("goldPerSec");
+		stats.Clicks = PlayerPrefs.GetFloat ("clicks");
+		stats.GoldMade = PlayerPrefs.GetFloat ("goldMade");
+		stats.BoughtAds = PlayerPrefs.GetInt ("purchasedAds");
+		stats.Diamonds = PlayerPrefs.GetInt ("diamonds");
+		stats.Item1.Count = PlayerPrefs.GetInt("item_number1");
+		stats.Item2.Count = PlayerPrefs.GetInt("item_number2");
+		stats.Item3.Count = PlayerPrefs.GetInt("item_number3");
+		stats.Upgrade1.Count = PlayerPrefs.GetInt("upgrade_number1");
+		stats.Upgrade2.Count = PlayerPrefs.GetInt("upgrade_number2");
+		stats.Upgrade3.Count = PlayerPrefs.GetInt("upgrade_number3");
+		stats.Message = (message == null)? "Loaded Game." : message ;
+
+		return stats;
 	}
 }
